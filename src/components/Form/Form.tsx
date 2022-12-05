36ddc7a4f007a974e02ce9446c286509a27d7a0f
Viet Mai <viet.mai@uac.edu.au>
2022-12-05 11:47:33 +0700

    recheck button

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,23 +2,21 @@ import React, { FormEvent, ReactNode } from "react";
 import "./index.css";
 
 interface FormProps {
-	title?: string | ReactNode;
-	subTitle?: string | ReactNode;
-	children: ReactNode;
-	onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-	submitLabel: string;
-	className: string;
-	buttonDisabled?: boolean;
+  title?: string | ReactNode;
+  subTitle?: string | ReactNode;
+  children: ReactNode;
+  onSubmit: (event: any) => void;
+  submitLabel: string;
+  className: string;
+  buttonDisabled?: boolean;
 }
 
 export default function Form(props: FormProps) {
-	return (
-		<div className={`form ${props.className ?? ""}`}>
-			{props.title && <h2 className="formTitle">{props.title}</h2>}
-			{props.subTitle && (
-				<h3 className="formSubtitle">{props.subTitle}</h3>
-			)}
-			<form onSubmit={props.onSubmit}>
+  return (
+    <div className={`form ${props.className ?? ""}`}>
+      {props.title && <h2 className="formTitle">{props.title}</h2>}
+      {props.subTitle && <h3 className="formSubtitle">{props.subTitle}</h3>}
+      {/* <form onSubmit={props.onSubmit}>
 				{props.children}
 				<input
 					disabled={props.buttonDisabled ?? false}
@@ -28,7 +26,14 @@ export default function Form(props: FormProps) {
 					type="submit"
 					value={props.submitLabel}
 				/>
-			</form>
-		</div>
-	);
+			</form> */}
+      <button
+        onClick={props.onSubmit}
+        className={`formSubmitButton ${props.buttonDisabled ? "disabled" : ""}`}
+        type="submit"
+      >
+        {props.submitLabel}
+      </button>
+    </div>
+  );
 }
